efb2162277b866a896d2670790e439a8f83a4399
Rugved <rugved.bongale@somaiya.edu>
2020-11-28 22:41:04 +0530

    Added times based OTP system using pyotp.TOTP

--- verification/urls.py
+++ verification/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import getPhoneNumberRegistered
+from .views import getPhoneNumberRegistered, getPhoneNumberRegistered_TimeBased
 
 urlpatterns = [
     path("<phone>/", getPhoneNumberRegistered.as_view(), name="OTP Gen"),
+    path("time_based/<phone>/", getPhoneNumberRegistered_TimeBased.as_view(), name="OTP Gen Time Based"),
 ]
